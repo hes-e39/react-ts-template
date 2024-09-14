32d4617ca96b1cac43664f4bd74212cca93ff2bf
Brandon Cloutier <bcloutier@protonmail.com>
2024-09-14 16:21:47 -0500

    Clean up components and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import reactLogo from '/react.svg';
 
 function App() {
     const [count, setCount] = useState<number>(1);
@@ -9,21 +7,17 @@ function App() {
     return (
         <div className="flex flex-col items-center gap-12 pt-32">
             <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
                 <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    <img src={reactLogo} className="size-32" alt="React logo" />
                 </a>
             </div>
-            <div className="text-3xl font-bold underline">Vite + React (test)</div>
+
+            <div className="text-3xl font-bold underline">E-39: Vite + React + TypeScript</div>
             <div>
-                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+                <button className="border-[0.5px] border-white p-2" onClick={() => setCount(count => count + 1)}>
+                    Click me: {count}
+                </button>
             </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </div>
     );
 }
